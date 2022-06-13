afaecf039145544dc7bc6bfaa5af8be58e62108b
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-06-13 11:00:50 +0430

    CodeChef problems

---
+++ CodeChefProblems/src/The_Three_Topics.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class The_Three_Topics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int x = sc.nextInt();
+        if (a == x || b == x || c == x) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
